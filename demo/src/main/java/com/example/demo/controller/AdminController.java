bfcc57434a1f914404b6d705af74c415766d3170
mkit <mkit@192.168.0.132>
2019-05-29 11:10:15 +0800

    mydemo

---
+++ demo/src/main/java/com/example/demo/controller/AdminController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {	
+	
+		
+	@RequestMapping("/admin/category")
+	public String indexdfdfsoesadadr() {
+		return "category";
+	}
+	
+	@RequestMapping("/admin/category/list")
+	public String indexlist() {
+		return "list";
+	}
+	
+	@RequestMapping("/admin/category/edit")
+	public String indexedit() {
+		return "edit";
+	}
+	
+}
+	
